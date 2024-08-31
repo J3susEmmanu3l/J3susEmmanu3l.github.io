c8e0a48750a1ccad326ab825a4ab28f175b31df8
JesusEmmanuelRuizLopez <159402090+J3susEmmanu3l@users.noreply.github.com>
2024-08-30 18:44:55 -0700

    Ruiz Lopez Jesus Emmanuel 30/08/2024

---
+++ BasedeDatosweb/Parcial1/holamundo.php
@@ -0,0 +1,17 @@
+<?php
+    $nombre = "Emmanuel";
+    $numero = 4;
+    $numero2 = "4";
+
+    $total = $numero + $numero2;
+
+    echo "Total = " . $total;
+    echo "<h1>Hola Mundo</h1>";
+
+    for($i=0;$i<10;$i++){
+        echo $i . "<br>";
+    }
+
+
+    echo "<h4 style= 'color:blue;'> Jesus Emmanuel Ruiz Lopez </h4>";
+?>
